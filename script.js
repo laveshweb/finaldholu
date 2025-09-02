875e7346f2657f2e33345f9a233a83ee9c297a11
laveh12 <laveshthecoder@gmail.com>
2025-09-01 17:04:00 -0700

    added about section

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelector(".slides");
+  const images = document.querySelectorAll(".slides img");
+  const total = images.length;
+
+  let current = 0;
+
+  setInterval(() => {
+    current = (current + 1) % total;
+    slides.style.transform = `translateX(-${current * 100}%)`;
+  }, 3000); // slide every 3 sec
+});
